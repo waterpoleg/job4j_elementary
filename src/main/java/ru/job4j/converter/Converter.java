e0c3cff198912a18cd49535237a585b46e451f63
Oleg Korneev <waterpoleg@gmail.com>
2021-10-28 22:18:07 +0300

    remove comments according to CI Bot notification

--- src/main/java/ru/job4j/converter/Converter.java
+++ src/main/java/ru/job4j/converter/Converter.java
@@ -13,13 +13,11 @@ public class Converter {
     public static void main(String[] args) {
         float in = 140;
 
-        // euro
         float expected = 2;
         float out = Converter.rubleToEuro(in);
         boolean passed = expected == out;
         System.out.println("140 rubles are 2. Test result : " + passed);
 
-        // usd
         expected = 2.3333333f;
         out = Converter.rubleToDollar(in);
         passed = expected == out;
